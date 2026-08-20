c4e810c314ad6bcc664e5d56e370ef7a302a0de2
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-20 14:15:49 +0200

    added serializable variable and javadoc in Frame class

--- src/fr/fms/graphic/Frame.java
+++ src/fr/fms/graphic/Frame.java
@@ -1,10 +1,28 @@
 package fr.fms.graphic;
 
 import javax.swing.*;
-
+/**
+ * Représente une fenetre d'affichage.
+ *
+ * <p>Attributs :</p>
+ * <ul>
+ *     <li>{@link} — Identifiant de version utilisé lors 
+ *     						 de la sérialisation de la classe.</li>
+ * </ul>
+ */
 public class Frame extends JFrame {
 
-    public Frame() {
+    /**
+	 * Identifiant de version utilisé lors de la sérialisation de la classe.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Crée et affiche la fenêtre principale de l'application.
+	 * Configure le titre, les dimensions, le panneau principal
+	 * et la fermeture de l'application.
+	 */
+	public Frame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Shapes challenge");
         setSize(350, 250);
