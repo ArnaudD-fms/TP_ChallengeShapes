fd4c27d24473b8b797d3777e3dab853d1944abfa
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-20 10:24:05 +0200

    added javadoc in square class

--- src/fr/fms/entities/Square.java
+++ src/fr/fms/entities/Square.java
@@ -1,10 +1,21 @@
 package fr.fms.entities;
+/**
+ * Représente un carré avec ses cotés
+ *
+ * <p>Attributs :</p>
+ * <ul>
+ *     <li>{@link #side} — longueur d'un coté</li>
 
+ * </ul>
+ */
 public class Square extends Shape{
-
+	/**
+     * longueur d'un coté
+     */
 	private int side;
 
 	/**
+	 * Construit un carré à partir de la longueur d'un coté
 	 * @param side
 	 */
 	public Square(int side, int x, int y) {
@@ -29,15 +40,21 @@ public class Square extends Shape{
 		this.side = side;
 		}
 	}
-	
+	/**
+	 * @param calcule l'aire du carré
+	 */
 	public double area() {
 		return this.side * this.side;
 	}
-	
+	/**
+	 * calcule le périmètre
+	 */
 	public double perimeter() {
 		return 4 * side;
 	}
-	
+	/**
+	 * Methode toString()
+	 */
 	@Override
 	public String toString() {
 		return "Square side : " + side + super.toString();
